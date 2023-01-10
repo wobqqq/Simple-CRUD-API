b81f33c86d0a374908004fe8057f0f137722983d
Sergey <s.zaharevich@itransition.com>
2023-01-10 18:11:06 +0300

    fix: User rules

--- src/http/validation/rules/UserRules.ts
+++ src/http/validation/rules/UserRules.ts
@@ -1,20 +1,20 @@
-export const showRules = {
+export const showRules = Object.freeze({
   id: 'required|uuid',
-};
+});
 
-export const createRules = {
+export const createRules = Object.freeze({
   username: 'required|string',
   age: 'required|number',
   hobbies: 'required|arrayOfStrings',
-};
+});
 
-export const updateRules = {
+export const updateRules = Object.freeze({
   id: 'required|uuid',
   username: 'required|string',
   age: 'required|number',
   hobbies: 'required|arrayOfStrings',
-};
+});
 
-export const deleteRules = {
+export const deleteRules = Object.freeze({
   id: 'required|uuid',
-};
+});
